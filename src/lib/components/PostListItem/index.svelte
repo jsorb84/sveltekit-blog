2688ff6f4565c014d1b5dcec0143552b0d506ede
jsorb84 <105475254+jsorb84@users.noreply.github.com>
2023-09-26 02:22:30 -0400

    up

---
+++ src/lib/components/PostListItem/index.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { Post } from '$lib/types/Post';
+	export let post: Post;
+</script>
+
+<a
+	href={`/post/${post.$id}`}
+	class="card card-hover variant-ghost container min-h-[200px] max-h-[300px]"
+>
+	<header class="card-header flex"><slot name="title" /></header>
+	<hr class="ml-3 mt-2 w-[65%]" />
+	<section class="px-6 m-auto h-max overflow-scroll"><slot /></section>
+	<footer class="card-footer mt-auto">fff</footer>
+</a>
--- src/lib/components/PostsList/index.svelte
+++ src/lib/components/PostsList/index.svelte
@@ -2,29 +2,22 @@
 	import type { Post } from '$lib/types/Post';
 	import type { User } from '$lib/types/User';
 	import Icon from '@iconify/svelte';
+	import PostItem from '$lib/components/PostListItem/index.svelte';
 	import { getContext } from 'svelte';
 	import type { Writable } from 'svelte/store';
 	export let posts: Post[];
 	let currentUser = getContext<Writable<User | null>>('currentUser');
 </script>
 
-{#each posts as post}
-	<div>
-		<h2 class="gradient-blue">{post.title}</h2>
-
-		<p class="px-4">{post.description}</p>
-		<div class="inline-flex gap-2">
-			<a href={`/post/${post.$id}`}
-				><button class="btn variant-outline-secondary">Read Article..</button></a
+<div class="grid grid-cols-3 gap-8">
+	{#each posts as post}
+		<PostItem {post}>
+			<span
+				slot="title"
+				class="gradient-text bg-gradient-to-r from-[#12c2e9] via-[#c471ed] to-[#f64f59] text-4xl font-bold"
+				>{post.title}</span
 			>
-			{#if $currentUser}
-				<a href={`/admin/edit/${post.$id}`}
-					><button class="btn variant-outline-tertiary"
-						><Icon icon="mdi:view-dashboard-edit" /> Edit Article</button
-					></a
-				>
-			{/if}
-		</div>
-	</div>
-	<hr />
-{/each}
+			<div class="font-light">{post.description}</div>
+		</PostItem>
+	{/each}
+</div>
